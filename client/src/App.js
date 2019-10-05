ed8a05adb0b571b4d0e389f2db1564f9ca03df25
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-05 00:21:03 -0400

    Make the MovieList card a Link that directs the user to a URL for that specific movie

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
+
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
