3be68c395c5d247e0a39eacbd13cae49786abaf3
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-07 10:04:55 -0400

    Refactor the movie card code into a separate component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import MovieCard from "./MovieCard.js";
+
 const MovieList = (props) => {
   const [movies, setMovies] = useState([]);
 
@@ -23,34 +25,18 @@ const MovieList = (props) => {
   return (
     <div className="movie-list">
       {movies.map((movie) => (
-        <Link to={`/movies/${movie.id}`}>
-          <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <MovieCard
+            key={movie.id}
+            title={movie.title}
+            director={movie.director}
+            metascore={movie.metascore}
+            stars={movie.stars}
+          />
         </Link>
       ))}
     </div>
   );
 };
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map((star) => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
 
 export default MovieList;
